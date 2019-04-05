0cde00cc82e63c3a82a921f188c37c8bc535323f
MunnCo <45236482+MunnCo@users.noreply.github.com>
2019-04-04 21:57:52 -0700

    Create skeleton storyline

---
+++ Packed/View/userRegistrationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  userRegistrationViewController.swift
+//  Packed
+//
+//  Created by Admin on 4/4/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class userRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+}
+
--- Packed/View/varsityTavernViewController.swift
+++ Packed/View/varsityTavernViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class varsityTavernViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
